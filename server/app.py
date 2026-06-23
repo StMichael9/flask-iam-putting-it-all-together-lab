161b43a94d4f60d4a058a59a939a0c92bfbafab9
St Michael Egenamba <michaelegenamba@gmail.com>
2026-06-23 00:40:14 -0700

    Passed all tests

--- server/app.py
+++ server/app.py
@@ -8,19 +8,63 @@ from config import app, db, api
 from models import User, Recipe, UserSchema, RecipeSchema
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json() or {}
+        username = data.get('username')
+        password = data.get('password')
+        image_url = data.get('image_url')
+        bio = data.get('bio')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return {"error": "User already exists"}, 422
+        
+        new_user = User(username=username, image_url = image_url, bio = bio)
+        new_user.password_hash = password
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        session["user_id"] = new_user.id
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return UserSchema().dump(user), 200
+        
+        return {}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json() or {}
+        username = data.get('username')
+        password = data.get('password')
+        
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+
+        return {'Error': "Unauthorized"}, 401
+            
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.clear()
+        return {}, 204
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+
+        recipes = Recipe.query.all()
+        seralized_recipes = RecipeSchema(many=True).dump(recipes)
+        if 'user_id' in session:
+            return seralized_recipes, 200
+        else: 
+            return {'Error': 'Unauthorized'}, 401
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -7,15 +7,48 @@ from config import db, bcrypt
 class User(db.Model):
     __tablename__ = 'users'
 
-    pass
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', backref = 'user')
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.Integer)
 
-class UserSchema(Schema):
-    pass
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class RecipeSchema(Schema):
-    pass
+    id = fields.Int()
+    title = fields.Str()
+    instructions = fields.Str()
+    minutes_to_complete = fields.Int()
+    user = fields.Nested(lambda: UserSchema(only=("id", "username")))
+
+
+class UserSchema(Schema):
+    id = fields.Int()
+    username = fields.Str()
+    image_url = fields.Str()
+    bio = fields.Str()
+    recipes = fields.List(fields.Nested(lambda: RecipeSchema()))
+
